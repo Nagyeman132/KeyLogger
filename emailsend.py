1b36afdc0ef133bdd5581c2cf210e7e959c4451a
Nagyeman132 <73144812+Nagyeman132@users.noreply.github.com>
2022-08-20 12:47:23 -0400

    Update emailsend.py

--- emailsend.py
+++ emailsend.py
@@ -1,21 +1,24 @@
-#generate new password via gmail each time 
-
+   
 from email.message import EmailMessage
 import ssl
 import smtplib
 
+
 email_sender = 'jw978537@gmail.com'
-email_password = ''
+email_password = 'twyf xava ilph etrx'
 email_receiver = 'jw978537@gmail.com'
 
-subject = 'Check out my code'
-body = 'CHECK MY LOGS'
-
+subject = 'New Log Update'
+body = 'Updated log from today'
+filename = 'logs.txt'
 email = EmailMessage()
 email['From'] = email_sender
 email['To'] = email_receiver
 email['Subject'] = subject
 email.set_content(body)
+
+email.add_attachment(open(filename, "r").read(), filename ="logs.txt")
+
 
 context = ssl.create_default_context()
 
